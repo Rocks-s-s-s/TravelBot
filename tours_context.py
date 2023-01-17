0b3d84c9cded9a7ec9c22acf72838336b5e1ecc3
Олег <oleg.martynov2008@gmail.com>
2023-01-17 17:06:01 +0300

    Решение домашнего задания № 2

--- tours_context.py
+++ tours_context.py
@@ -1,5 +1,5 @@
 def read_file(filename):
-    file = open('tours.csv', encoding='utf-8')
+    file = open(filename, encoding='utf-8')
     data = []
     for line in file:
         data.append(line.strip().split(','))
@@ -8,5 +8,20 @@ def read_file(filename):
 def get_types_rest(filename):
     # читаем данные из файла
     filedata = read_file(filename)
+    uniquetype = []
+    first = 0
     for line in filedata:
-        pass
+        x = 0
+        if first == 0:
+            first = 1
+            continue
+        for i in range(len(uniquetype)):
+            if uniquetype[i] != line[3]:
+                x = x + 1
+        if x == len(uniquetype):
+            uniquetype.append(line[3])
+    return uniquetype
+
+filename = "tours.csv"
+uniquetype = get_types_rest(filename)
+print(uniquetype)
