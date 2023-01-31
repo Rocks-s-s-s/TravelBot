22b9742b7eca16cbd1f163c0e5a50f6ad7cb53de
Олег <oleg.martynov2008@gmail.com>
2023-01-31 17:01:12 +0300

    Выполнение дз № 7(1,2 почти сделано)

--- bot.py
+++ bot.py
@@ -10,42 +10,17 @@
 """
 # Такое подключение лучше, т.к. не нужно писать название модуля, а просто название функции
 from tours_context import *
+from UserAction import *
+print("Как вы хотите использовать программу")
+print("1 - Найти тур")
+print("2 - Добавить новый тур")
+print("3 - Показать все туры")
+print("0 - Выход")
 
-filename = 'tours.csv'
-type = get_types_rest(filename)
-num = 0
-corest_imput = 0
-typs = []
-save_type = 0
-countries = []
-print('Выберите тип отдыха(для выбора типа отдыха введите его номер):')
-for line in type:
-    num += 1
-    typs.append(line)
-    print('%d - %s' % (num, line))
-while corest_imput == 0:
-    choice = int(input())
-    if choice <= len(typs):
-        print("Выбран тип отдыха -", typs[choice - 1])
-        save_type = choice
-        corest_imput = 1
-    else:
-        print("Некоректный ввод")
-corest_imput = 0
-coutry = get_country_by_type(filename, type=typs[choice - 1])
-print('Выберите страну(для выбора страны  введите её номер):')
-num = 0
-for line in coutry:
-    num += 1
-    countries.append(line)
-    print('%d - %s' % (num, line))
-while corest_imput == 0:
-    choice = int(input())
-    if choice <= len(countries):
-        print("Выбраная страна -", countries[choice - 1])
-        corest_imput = 1
-    else:
-        print("Некоректный ввод")
-
-tours = get_tours(filename, type=typs[save_type - 1], country=countries[choice - 1])
-# print("Програма нашла вам подходяший тур - %s" % (tours))
+choice = int(input())
+if choice == 1:
+    tours = []
+    tours = find_a_tour()
+    format_print_tour(tours)
+if choice == 3:
+    find_all()
